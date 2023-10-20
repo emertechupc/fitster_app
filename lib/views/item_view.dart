597d6b980711a07182eebff7723961b8ae947d63
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-10-20 13:38:51 -0500

    refactor: fix design

--- lib/views/item_view.dart
+++ lib/views/item_view.dart
@@ -27,69 +27,80 @@ class _ItemViewState extends State<ItemView> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const Image(image: AssetImage('assets/men1.jpg')),
-              const Row(
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Men's t-shirt",
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Men's t-shirt",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          '\$29.90',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 8),
-                  Text(
-                    '\$29.90',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20.0,
+                  RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: false,
+                    itemCount: 5,
+                    itemSize: 20.0,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                    itemBuilder: (context, _) => const Icon(
+                      Icons.star,
+                      color: Colors.yellow,
                     ),
+                    onRatingUpdate: (rating) {
+                      if (kDebugMode) {
+                        print(rating);
+                      }
+                    },
                   ),
                 ],
               ),
-              RatingBar.builder(
-                initialRating: 3,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: false,
-                itemCount: 5,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.black,
-                ),
-                onRatingUpdate: (rating) {
-                  if (kDebugMode) {
-                    print(rating);
-                  }
-                },
-              ),
+              SizedBox(height: 10,),
               const Text(
                 'Nike Sportswear',
                 style: TextStyle(
                   fontSize: 18.0,
-                  color: Colors.black,
                   fontWeight: FontWeight.normal,
                 ),
               ),
+              SizedBox(height: 10,),
               const Text(
                 'Made from our soft, midweight cotton in a relaxed fit, this tee keeps it casual and comfy all day. Bold graphics celebrate our diverse global community and the connections we make through sport.',
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 14.0,
-                  color: Colors.black,
                   fontWeight: FontWeight.w300,
                 ),
               ),
+              SizedBox(height: 10,),
               const Text(
                 'Select size',
                 style: TextStyle(
                   fontSize: 18.0,
-                  color: Colors.black,
                   fontWeight: FontWeight.normal,
                 ),
               ),
-              ButtonBar(
+              SizedBox(height: 10,),
+              OverflowBar(
+                alignment: MainAxisAlignment.center,
                 children: ['XS', 'S', 'M', 'L', 'XL']
                     .map(
                       (size) => _SizeButton(
@@ -99,6 +110,7 @@ class _ItemViewState extends State<ItemView> {
                     )
                     .toList(),
               ),
+              SizedBox(height: 10,),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -129,18 +141,21 @@ class _SizeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        shape: MaterialStatePropertyAll<OutlinedBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
+    return Padding(
+      padding: const EdgeInsets.only(left: 5.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStatePropertyAll<OutlinedBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+            ),
           ),
         ),
-      ),
-      onPressed: onPressed,
-      child: DefaultTextStyle(
-        style: const TextStyle(fontSize: 14),
-        child: child,
+        onPressed: onPressed,
+        child: DefaultTextStyle(
+          style: const TextStyle(fontSize: 14),
+          child: child,
+        ),
       ),
     );
   }
