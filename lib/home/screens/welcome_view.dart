8ff7744f43c077b2d5e8847bd2db05b09b2e6435
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-10-27 13:06:00 -0500

    feat: brands list view implementation

--- lib/home/screens/welcome_view.dart
+++ lib/home/screens/welcome_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../service/item_service.dart';
 import 'ad_slidshow.dart';
+import 'brands_horizontal_listview.dart';
 import 'item_horizontal_listview.dart';
 
 class WelcomeView extends StatelessWidget {
@@ -12,41 +12,58 @@ class WelcomeView extends StatelessWidget {
     'assets/ads/anuncio3.jpg',
   ];
 
+  static const List<String> brands = [
+    'assets/brands/adidas.svg',
+    'assets/brands/gucci.svg',
+    'assets/brands/nike.svg',
+    'assets/brands/puma.svg',
+    'assets/brands/ralphlauren.svg',
+    'assets/brands/tommy-hilfiger.svg',
+  ];
+
   const WelcomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     final itemService = Provider.of<ItemService>(context);
     final itemFuture = itemService.getProduct();
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 10.0,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              AdSlideshow(
+                ads: ads,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FutureBuilder(
+                future: itemFuture,
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return ItemHorizontalListview(
+                      items: snapshot.data,
+                      title: 'Most popular products',
+                    );
+                  } else {
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: Center(child: CircularProgressIndicator()),
+                    );
+                  }
+                },
+              ),
+              BrandsHorizontalListview(brands: brands, title: 'Associated brands',),
+              SizedBox(height: 20,)
+            ],
           ),
-          AdSlideshow(
-            ads: ads,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FutureBuilder(
-            future: itemFuture,
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return ItemHorizontalListview(
-                  items: snapshot.data,
-                  title: 'Most popular products',
-                );
-              } else {
-                return SizedBox(
-                  height: MediaQuery.of(context).size.height / 3,
-                  child: Center(child: CircularProgressIndicator()),
-                );
-              }
-            },
-          )
-        ],
+        ),
       ),
     );
   }
