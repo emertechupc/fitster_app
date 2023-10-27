b2cbcf182db1ee5c36a403761d6c1030404c9247
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-10-27 16:22:01 -0500

    feat: widget for shopping cart

--- lib/shopping/screens/item_shopping.dart
+++ lib/shopping/screens/item_shopping.dart
@@ -5,8 +5,65 @@ class ItemShopping extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-
+    final colors = Theme.of(context);
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: colors.dividerColor,
+          ),
+        ),
+      ),
+      height: 180,
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 10.0),
+            height: 150,
+            width: 120,
+            color: Colors.amber,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'T-shirt',
+                    maxLines: 2,
+                  ),
+                  Text(
+                    'White Casual Wear Blazer for women',
+                    maxLines: 2,
+                  ),
+                  Text('Size M'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('S/. 250'),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text('1'),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
