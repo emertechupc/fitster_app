5c6ab4a9ad03d243b3e5e72c8e2046d77eb29eec
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-11-01 15:56:11 -0500

    feat: add routing

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:provider/provider.dart';
 import 'auth/screens/signup_view.dart';
 import 'home/screens/main_bounce_tab_bar.dart';
 import 'home/service/item_service.dart';
+import 'profile/screens/user_profile_view.dart';
 import 'shopping/screens/confirmed_status_view.dart';
 import 'shopping/screens/order_view.dart';
 import 'states/theme_state.dart';
-import 'auth/screens/home_page.dart';
+import 'auth/screens/signin_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,11 +33,12 @@ class FitsterApp extends StatelessWidget {
           return MaterialApp(
             initialRoute: 'signin',
             routes: {
-              'signin': (_) => HomePage(),
-              'signup': (_) => SignUpView(),
-              'home': (_) => MainPage(),
-              'order': (_) => OrderView(),
-              'confirmed': (_) => ConfirmedStatusView(),
+              'signin'    : (_) => SignInView(),
+              'signup'    : (_) => SignUpView(),
+              'home'      : (_) => MainPage(),
+              'order'     : (_) => OrderView(),
+              'confirmed' : (_) => ConfirmedStatusView(),
+              'profile'   : (_) => UserProfileView(),
             },
 
             title: 'Fitster',
