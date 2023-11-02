1649da07215b3ce1537cfd71c06fd5f22e213300
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-11-01 20:59:03 -0500

    feat: add model for user

---
+++ lib/auth/model/user.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+class User {
+  int? id;
+  String firstName;
+  String lastName;
+  String email;
+  String? password;
+
+  User({
+    this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    this.password,
+  });
+
+  User copyWith({
+    int? id,
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? password,
+  }) =>
+      User(
+        id: id ?? this.id,
+        firstName: firstName ?? this.firstName,
+        lastName: lastName ?? this.lastName,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json['id'],
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        email: json['email'],
+        password: json['password'],
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'password': password,
+      };
+}
