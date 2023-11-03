de83687c57e2b81be3835e3ec1ad3874bb59e1de
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-11-03 13:16:05 -0500

    faet: wish list implementation

--- lib/utils/item_list.dart
+++ lib/utils/item_list.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 import '../search/screens/clothing_list_view.dart';
 
 class _Item extends StatelessWidget {
-  final String child;
+  final String? child;
+  final int genderId;
+  final int brandId;
+  final int categoryId;
+  final bool isGenderId;
+  final bool isBrandId;
+  final bool isCategoryId;
 
-  const _Item({required this.child});
+  const _Item({
+    required this.child,
+    required this.isGenderId,
+    required this.genderId,
+    required this.isBrandId,
+    required this.brandId,
+    required this.isCategoryId,
+    required this.categoryId,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +28,14 @@ class _Item extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => const ClothingListView(),
+            builder: (context) => ClothingListView(
+              isBrandId: isBrandId,
+              brandId: brandId,
+              categoryId: categoryId,
+              genderId: genderId,
+              isCategoryId: isCategoryId,
+              isGenderId: isGenderId,
+            ),
           ),
         );
       },
@@ -30,7 +51,7 @@ class _Item extends StatelessWidget {
         ),
         child: Center(
           child: Text(
-            child,
+            child!,
             style: const TextStyle(
               fontSize: 14.0,
               color: Color(0xFF4B64F2),
@@ -42,26 +63,45 @@ class _Item extends StatelessWidget {
   }
 }
 
-class ItemList extends StatefulWidget {
-  final List<String> items;
-  const ItemList({super.key, required this.items});
+class ItemMap extends StatefulWidget {
+  final Map<int, String> items;
+  final bool isGenderId;
+  final bool isBrandId;
+  final bool isCategoryId;
+  const ItemMap({
+    super.key,
+    required this.items,
+    required this.isBrandId,
+    required this.isCategoryId,
+    required this.isGenderId,
+  });
 
   @override
-  State<ItemList> createState() => _ItemListState();
+  State<ItemMap> createState() => _ItemMapState();
 }
 
-class _ItemListState extends State<ItemList> {
+class _ItemMapState extends State<ItemMap> {
   @override
   Widget build(BuildContext context) {
+    print(widget.isGenderId);
     return SizedBox(
       height: MediaQuery.of(context).size.width / 2,
       child: ListView.builder(
         physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,      
+        scrollDirection: Axis.horizontal,
         shrinkWrap: true,
         itemCount: widget.items.length,
         itemBuilder: (context, index) {
-          return _Item(child: widget.items[index]);
+          int key = widget.items.keys.elementAt(index);
+          return _Item(
+            child: widget.items[key],
+            isGenderId: widget.isGenderId,
+            isBrandId: widget.isBrandId,
+            isCategoryId: widget.isCategoryId,
+            genderId: (index + 1),
+            categoryId: (index + 1),
+            brandId: (index + 1),
+          );
         },
         padding: const EdgeInsets.all(8),
       ),
